90fc20f34a0cd4a10559e83e16ed8c6b801a676a
krissh-the-dev <akrishnamoorthy007@gmail.com>
2025-08-15 10:07:42 +0530

    fish: added tab completions for starship

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,6 +5,7 @@ if status --is-interactive
 
 	source ~/.config/shell/init.fish
 	source ~/.config/shell/integrations.fish
+	source ~/.config/shell/completions/starship.fish
 end
 
 if status --is-login
