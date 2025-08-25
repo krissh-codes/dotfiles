615b59008a0af00265fe5ba74cc7554e1d19c12c
krissh-the-dev <akrishnamoorthy007@gmail.com>
2025-08-25 07:53:49 +0530

    shell: used relevative paths for env

--- shell/.config/shell/environment.sh
+++ shell/.config/shell/environment.sh
@@ -6,14 +6,16 @@ export MYSQL_PATH=/usr/local/mysql/bin
 export PSQL_PATH=/Applications/Postgres.app/Contents/Versions/latest/bin
 export PATH=$PATH:$PSQL_PATH:$BREW_PATH:$MYSQL_PATH
 export PATH="$PATH:~/node_modules/.bin":"./node_modules/grunt-cli/bin"
-export PG_DATA=/Users/krishna-13961/Library/Application\ Support/Postgres/var-9.5
-export ECLIPSEZIDEPATH=/Users/krishna-13961/dev/
-export PATH=$PATH:/Users/krishna-13961/zoho/scripts:$BREW_BIN
+export PG_DATA=~/Library/Application\ Support/Postgres/var-9.5
+export ECLIPSEZIDEPATH=~/dev/
+export PATH=$PATH:~/zoho/scripts:$BREW_BIN
 
 export LDFLAGS="-L/opt/homebrew/opt/postgresql@9.5/lib"
 export CPPFLAGS="-I/opt/homebrew/opt/postgresql@9.5/include"
-export STARSHIP_CONFIG="/Users/krishna-13961/.config/starship/config.toml"
-export STARSHIP_CACHE="/Users/krishna-13961/.config/starship/cache"
+
+# starship
+export STARSHIP_CONFIG=~/.config/starship/config.toml
+export STARSHIP_CACHE=~/.config/starship/cache
 
 # bun
 export BUN_INSTALL="$HOME/.bun"
