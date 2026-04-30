172ab3e60a9e9a5ba1d9ea93948b0dd8ccd026d1
krissh-codes <akrishnamoorthy007@gmail.com>
2026-04-30 08:04:07 +0530

    shell: fixed asdf shell integration

--- shell/.config/shell/environment.sh
+++ shell/.config/shell/environment.sh
@@ -1,5 +1,8 @@
-source /opt/homebrew/opt/asdf/libexec/asdf.sh
+# source /opt/homebrew/opt/asdf/libexec/asdf.sh
+export ASDF_DATA_DIR="$HOME/.asdf"
 export JAVA_HOME=$HOME/.asdf/installs/java/zulu-17.62.17/zulu-17.jdk/Contents/Home/
+ASDF_BIN="$ASDF_DATA_DIR/bin"
+ASDF_SHIMS="$ASDF_DATA_DIR/shims"
 
 export BREW_PATH=/opt/homebrew/bin/brew
 export BREW_BIN=/opt/homebrew/bin
@@ -13,7 +16,7 @@ export GRUNT_PATH=~/node_modules/grunt-cli/bin
 export BUN_INSTALL="$HOME/.bun"
 export BUN_PATH="$BUN_INSTALL/bin"
 
-export PATH=$PATH:$PSQL_PATH:$BREW_PATH:$BREW_BIN:$MYSQL_PATH:$NPM_GLOBAL:$GRUNT_PATH:$BUN_PATH
+export PATH=$PATH:$PSQL_PATH:$BREW_PATH:$BREW_BIN:$MYSQL_PATH:$NPM_GLOBAL:$GRUNT_PATH:$BUN_PATH:$ASDF_BIN:$ASDF_SHIMS
 
 export ECLIPSEZIDEPATH=~/dev/
 
@@ -23,3 +26,5 @@ export CPPFLAGS="-I/opt/homebrew/opt/postgresql@9.6/include"
 # starship
 export STARSHIP_CONFIG=~/.config/starship/config.toml
 export STARSHIP_CACHE=~/.config/starship/cache
+
+export NODE_EXTRA_CA_CERTS="$HOME/.config/cert/corporate-ca.pem"
